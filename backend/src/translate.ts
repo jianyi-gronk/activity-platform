831ee4e4ec808a53bff8853c4744686e65a570bd
jianyi-gronk <jianyi_gronk@163.com>
2024-01-30 23:04:42 +0800

    add backend

--- backend/src/translate.ts
+++ backend/src/translate.ts
@@ -1,11 +1,15 @@
-const convert = require("google-translate-api");
+const translate = require("translate");
 
-export function translate(value) {
-  convert(value, { to: "en" })
-    .then((res) => {
-      return res.text;
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-}
+translate.engine = "google";
+translate.key = "YOUR_API_KEY";
+
+const textToTranslate = "要翻译的文本";
+const targetLanguage = "en";
+
+translate(textToTranslate, targetLanguage)
+  .then((translatedText) => {
+    console.log(translatedText);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
