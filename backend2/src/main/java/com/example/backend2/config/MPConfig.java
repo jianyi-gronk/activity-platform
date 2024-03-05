245d62a8a27c0a91120fcd313c24edf7e7079793
0408葛瑶 <916586595@qq.com>
2024-03-06 00:10:31 +0800

    配置文件与接口文档完善

---
+++ backend2/src/main/java/com/example/backend2/config/MPConfig.java
@@ -0,0 +1,16 @@
+package com.example.backend2.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MPConfig {
+    @Bean
+    MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor()); // 分页拦截器
+        return mybatisPlusInterceptor;
+    }
+}
